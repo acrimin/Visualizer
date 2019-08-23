fcd986d6c4b854a176b8b222246dfe14132687c8
Drew Criminski <acriminski@gmail.com>
2019-08-22 21:44:54 -0500

    Moving to using volumes instead of pitch, have averages setup, next step sensing beats

--- sketchAmp.js
+++ sketchAmp.js
@@ -1,44 +1,100 @@
-var song;
-var amp;
-var button;
+var objMic;
+var objAmp;
 
-var volHistory = [];
+const _nBeatTime = 0;
 
-function toggleSong()
-{
-	if (song.isPlaying())
-	{
-		song.pause();
-	}
-	else
-	{
-		song.play();
-	}
-}
+const _nMinLevel = 0.03;
 
-function preload() 
-{
-	song = loadSound("song.mp3");
-}
+const _nLevelThreshold = 0.5;
+const _nAverageTrackTime = 60 * 1000;
+// const _nAverageTrackResolutionTime = 1000;
+const _nAverageTrackResolutionCount = 60;
+var _nAverage = 1;
+var _nSumAverage = 1;
+var _aAverageLevelsHolder = [1];
+
+// var _nAverageTrackTime = _nAverageTrackResolutionTime * _nAverageTrackResolutionCount;
+var _nAverageTrackResolutionTime = _nAverageTrackTime / _nAverageTrackResolutionCount;
+// var _nAverageTrackResolutionCount = _nAverageTrackTime / _nAverageTrackResolutionTime;
+
+var nPrevTime = -1 * _nAverageTrackResolutionTime;
 
 function setup() {
-	createCanvas(200, 200);
-	button = createButton("toggle");
-	button.mousePressed(toggleSong);
-	amp = new p5.Amplitude();
+	createCanvas(600, 400);
+
+	objMic = new p5.AudioIn();
+	objMic.start();
+
+	objAmp = new p5.Amplitude();
+	objAmp.setInput(objMic);
 }
 
 function draw() {
 	background(0);
-	var vol = amp.getLevel();
 
-	volHistory.push(vol);
-	stroke(255);
+	var nVolume = objAmp.getLevel();
+
+	if (nVolume > _nMinLevel)
+	{
+		stroke(255);
+		fill(0,255,0);
+		rect(0, 0, 20, 20);
+
+		var nTime = millis();
+		if (nTime > nPrevTime + _nAverageTrackResolutionTime || nPrevTime === -1)
+		{
+			nPrevTime = nTime;
+			calcAverages(nVolume);
+		}
+	}
+	else
+	{
+		stroke(255);
+		fill(255,0,0);
+		rect(0, 0, 20, 20);
+	}
+
+	drawHistory(nVolume);
+	drawLevel(nVolume);
+}
+
+function drawLevel(nVolume)
+{
+	noStroke();
+	fill(0,255,0);
+
+	var value = map(nVolume, 0, 1, 0, width);
+	rect(0, 200, value, 20);
+
+	fill(255,0,0);
+	var value = map(nVolume, 0, _nAverage / _nLevelThreshold, 0, width);
+	rect(0, 230, value, 20);
+
+	fill(0,0,255);
+	var value = map(_nAverage, 0, 1, 0, width);
+	rect(0, 260, value, 20);
+}
+
+var volHistory = [];
+function drawHistory(volume)
+{
+	volHistory.push(volume);
+	stroke(0, 255, 0);
 	noFill();
 	beginShape();
 	for (var i = 0; i < volHistory.length; i++)
 	{
-		var y = map(volHistory[i], 0, 1, height, 0);
+		var y = map(volHistory[i], 0, 1, 200, 0);
+		vertex(i, y);
+	}
+	endShape();
+
+	stroke(255, 0, 0);
+	noFill();
+	beginShape();
+	for (var i = 0; i < volHistory.length; i++)
+	{
+		var y = map(volHistory[i], 0, _nAverage / _nLevelThreshold, 200, 0);
 		vertex(i, y);
 	}
 	endShape();
@@ -47,4 +103,18 @@ function draw() {
 	{
 		volHistory.splice(0, 1);
 	}
+}
+
+function calcAverages(nVolume)
+{
+	_aAverageLevelsHolder.push(nVolume);
+	_nSumAverage += nVolume;
+
+	if (_aAverageLevelsHolder.length > _nAverageTrackResolutionCount)
+	{
+		var nRemoved = _aAverageLevelsHolder.shift();
+		_nSumAverage -= nRemoved;
+	}
+
+	_nAverage = _nSumAverage / _aAverageLevelsHolder.length;
 }
